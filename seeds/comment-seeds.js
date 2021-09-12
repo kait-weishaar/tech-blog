4a2bd2a3697dbfc6af579d369b8e48be846ed50c
Kait <kweishaar@hawk.iit.edu>
2021-09-11 20:17:21 -0500

    seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,263 +2,263 @@ const { Comment } = require('../models');
 
 const commentdata = [
   {
-    comment_text: 'Nunc rhoncus dui vel sem.',
+    comment_text: 'So rad',
     user_id: 6,
     post_id: 1
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'This is the best',
     user_id: 6,
     post_id: 8
   },
   {
-    comment_text: 'Aliquam erat volutpat. In congue.',
+    comment_text: 'I love it',
     user_id: 3,
     post_id: 10
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'Great Post!',
     user_id: 3,
     post_id: 18
   },
   {
-    comment_text: 'In hac habitasse platea dictumst.',
+    comment_text: 'Fantastic Content, thanks for posting',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Vivamus vestibulum sagittis sapien.',
+    comment_text: 'Lovely',
     user_id: 1,
     post_id: 20
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'Hate it',
     user_id: 6,
     post_id: 7
   },
   {
-    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
+    comment_text: 'Useless',
     user_id: 7,
     post_id: 4
   },
   {
-    comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    comment_text: 'I dont get it',
     user_id: 6,
     post_id: 12
   },
   {
-    comment_text: 'Morbi a ipsum.',
+    comment_text: 'Not true',
     user_id: 6,
     post_id: 20
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'Not helpful',
     user_id: 3,
     post_id: 14
   },
   {
-    comment_text: 'Donec ut mauris eget massa tempor convallis.',
+    comment_text: 'Great point! We should talk some time',
     user_id: 5,
     post_id: 4
   },
   {
     comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+      'Intersting',
     user_id: 4,
     post_id: 9
   },
   {
     comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+      'Sure',
     user_id: 5,
     post_id: 14
   },
   {
-    comment_text: 'Quisque porta volutpat erat.',
+    comment_text: 'Why not',
     user_id: 6,
     post_id: 2
   },
   {
-    comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    comment_text: 'Yuppers',
     user_id: 8,
     post_id: 2
   },
   {
     comment_text:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+      'Yikes',
     user_id: 2,
     post_id: 20
   },
   {
-    comment_text: 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
+    comment_text: 'Not convinced',
     user_id: 4,
     post_id: 11
   },
   {
     comment_text:
-      'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+      'Mind blown.',
     user_id: 5,
     post_id: 13
   },
   {
     comment_text:
-      'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+      'Earth shattering.',
     user_id: 9,
     post_id: 16
   },
   {
-    comment_text: 'Curabitur convallis.',
+    comment_text: 'No bueno',
     user_id: 6,
     post_id: 4
   },
   {
-    comment_text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    comment_text: 'Por que?',
     user_id: 4,
     post_id: 10
   },
   {
-    comment_text: 'Morbi non quam nec dui luctus rutrum.',
+    comment_text: 'Asi Asi',
     user_id: 3,
     post_id: 8
   },
   {
     comment_text:
-      'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
+      'Que rico',
     user_id: 8,
     post_id: 10
   },
   {
     comment_text:
-      'Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+      'Estoy descansada',
     user_id: 1,
     post_id: 15
   },
   {
-    comment_text: 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.',
+    comment_text: 'Que asco',
     user_id: 5,
     post_id: 3
   },
   {
-    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    comment_text: 'No es para me',
     user_id: 1,
     post_id: 15
   },
   {
-    comment_text: 'Nam tristique tortor eu pede.',
+    comment_text: 'Ningun.',
     user_id: 4,
     post_id: 16
   },
   {
-    comment_text: 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    comment_text: 'Just okay.',
     user_id: 4,
     post_id: 18
   },
   {
-    comment_text: 'Proin eu mi. Nulla ac enim.',
+    comment_text: 'Its for the best.',
     user_id: 4,
     post_id: 10
   },
   {
-    comment_text: 'Sed ante. Vivamus tortor.',
+    comment_text: 'Mellow yellow.',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Aliquam quis turpis eget elit sodales scelerisque.',
+    comment_text: 'Idk.',
     user_id: 10,
     post_id: 1
   },
   {
-    comment_text: 'Donec quis orci eget orci vehicula condimentum.',
+    comment_text: 'Grammar',
     user_id: 3,
     post_id: 19
   },
   {
-    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    comment_text: 'Spelling mistake.',
     user_id: 5,
     post_id: 3
   },
   {
-    comment_text: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',
+    comment_text: 'So true.',
     user_id: 10,
     post_id: 14
   },
   {
-    comment_text: 'Maecenas ut massa quis augue luctus tincidunt.',
+    comment_text: 'For real.',
     user_id: 10,
     post_id: 8
   },
   {
-    comment_text: 'Cras in purus eu magna vulputate luctus.',
+    comment_text: 'I guess.',
     user_id: 10,
     post_id: 11
   },
   {
-    comment_text: 'Etiam vel augue. Vestibulum rutrum rutrum neque.',
+    comment_text: 'Meh.',
     user_id: 8,
     post_id: 5
   },
   {
-    comment_text: 'Proin at turpis a pede posuere nonummy.',
+    comment_text: 'Waste of time.',
     user_id: 8,
     post_id: 19
   },
   {
-    comment_text: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
+    comment_text: 'Boring.',
     user_id: 9,
     post_id: 19
   },
   {
     comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+      'So many comments so little time',
     user_id: 5,
     post_id: 4
   },
   {
     comment_text:
-      'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
+      'This is copyrighted.',
     user_id: 2,
     post_id: 11
   },
   {
-    comment_text: 'Vestibulum ac est lacinia nisi venenatis tristique.',
+    comment_text: 'Not entirely true',
     user_id: 4,
     post_id: 6
   },
   {
-    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    comment_text: 'Im skeptical',
     user_id: 9,
     post_id: 6
   },
   {
     comment_text:
-      'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.',
+      'Hmmm. Interesting',
     user_id: 7,
     post_id: 9
   },
   {
-    comment_text: 'Integer ac leo. Pellentesque ultrices mattis odio.',
+    comment_text: 'So many words',
     user_id: 4,
     post_id: 19
   },
   {
-    comment_text: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+    comment_text: 'My opinion.',
     user_id: 10,
     post_id: 1
   },
   {
     comment_text:
-      'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.',
+      'Two cents.',
     user_id: 2,
     post_id: 19
   },
   {
-    comment_text: 'Proin risus. Praesent lectus.',
+    comment_text: 'Sounds great',
     user_id: 10,
     post_id: 1
   },
   {
-    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.',
+    comment_text: 'Cool',
     user_id: 10,
     post_id: 12
   }
